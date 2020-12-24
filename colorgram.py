bc7695f5bfd7ccdfe6074101c6cd517f8f69549e
dandrea1 <47754826+dandrea1@users.noreply.github.com>
2020-12-24 10:00:09 -0500

    Create colorgram.py

---
+++ colorgram.py
@@ -0,0 +1,13 @@
+# import colorgram
+#
+# colors = colorgram.extract('image.jpg', 20)
+#
+# rgb_list = []
+#
+# for color in colors:
+#     initial_tuple = color.rgb
+#     converted_list = list(initial_tuple)
+#     final_tuple = tuple(converted_list)
+#     rgb_list.append(final_tuple)
+#
+# print(rgb_list)
